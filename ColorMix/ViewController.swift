6c8738624e99749465ff2a5b8e64784429f7c2bb
Jonathan E. Hardis <jhardis@me.com>
2021-10-14 14:56:33 -0400

    End of Part 1

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        colorView.backgroundColor = .black
     }
 
 
